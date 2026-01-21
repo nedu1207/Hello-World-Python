2ef9e1497384c3aad2c6a0c748f8a572ea5b6c85
nedu1207 <chineduiwu2007@gmail.com>
2026-01-21 20:11:48 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,18 +1,1 @@
-""  
-def caeser_encrypt(text,shift):
-    print(f"Input text: {text}")
-    print(f"Shift value: {shift}")
-    result = "omo"
-    for char in text:
-        if char.isalpha():
-            ascii_offset = 65 if char.isupper() else 97
-            result += chr((ord)-ascii_offset+shift)%26 + ascii_offset
-        else:
-            result += char
-            return result
-        print (f"Encrypted text: {result}")
-        return result
-        text = ""
-        shift = 3
-        encrypted = caeser_encrypt(text,shift)
-        print(encrypted)
+print("Hello World!!!")
